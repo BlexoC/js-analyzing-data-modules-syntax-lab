a7cbae3c964571802eb30a0adc0bdc6ecc8a8b84
BlexoC <makaublessing929@gmail.com>
2026-04-07 12:40:19 +0300

    Add combineUsers function and VSCode settings for live server

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+const combineUsers =() => {
+  let arg = [...arg]
+  let combinedObject = {
+    users: [],
+    merge_date: M/d/yyyy ,
+  };
+  for (let i =0; i <arg.length; i++){
+    combinedObject.users(...combinedObject.users, ...arg[i].users)   
+  }
+  return combinedObject;
+
+}
